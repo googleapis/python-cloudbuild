5edb894cba72c56c52a0b3b3f2a275032f8edaa4
Josh Moore <joshuamo@google.com>
2022-08-03 12:47:35 -0500

    Add example of using client_options for regional endpoints

--- samples/snippets/quickstart.py
+++ samples/snippets/quickstart.py
@@ -16,6 +16,7 @@
 # [START cloudbuild_quickstart]
 import google.auth
 from google.cloud.devtools import cloudbuild_v1
+from google.api_core import client_options
 
 
 def quickstart():
@@ -23,10 +24,16 @@ def quickstart():
     print the in-progress status and print the completed status."""
 
     # Authorize the client with Google defaults
-    # If you're using Private Pools, add a regional `api_endpoint` to `CloudBuildClient()`
-    # For example, '<YOUR_POOL_REGION>-cloudbuild.googleapis.com'
     credentials, project_id = google.auth.default()
     client = cloudbuild_v1.services.cloud_build.CloudBuildClient()
+
+	# If you're using Private Pools or a non-global default pool, add a regional
+    # `api_endpoint` to `CloudBuildClient()`
+    # For example, '<YOUR_POOL_REGION>-cloudbuild.googleapis.com'
+    # client_options = client_options.ClientOptions(
+    #     api_endpoint="us-central1-cloudbuild.googleapis.com"
+    # )
+    # client = cloudbuild_v1.services.cloud_build.CloudBuildClient(client_options=client_options)
 
     build = cloudbuild_v1.Build()
 
